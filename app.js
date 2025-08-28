88e029c28a22b02f4869d690845e8be4519127a2
PrivateBot <82006592+ianvanh@users.noreply.github.com>
2025-08-27 23:20:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const BASE_URL = 'https://api.betmines.com/betmines/v1';
 
